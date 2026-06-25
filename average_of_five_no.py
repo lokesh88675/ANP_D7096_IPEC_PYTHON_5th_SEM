4c16da3dce5ad3a993c207d1e836aa1876f7f56b
lokesh88675 <lokeshndaair1@gmail.com>
2026-06-25 12:49:11 +0530

    average of five no

---
+++ average_of_five_no.py
@@ -0,0 +1,10 @@
+# taking  no from user
+num1 = float(input("enter your first no"))
+num2 = float(input("enter your second no"))
+num3 = float(input("enter your third no"))
+num4 = float(input("enter your fourth no"))
+num5= float(input("enter your fifth no"))
+
+#now doing average of this no
+avrg = (num1 +num2 +num3 +num4 +num5)/5
+print("average of five no is",avrg)

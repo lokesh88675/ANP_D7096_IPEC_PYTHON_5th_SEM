99c967f4717546eef22e76eb55e1a82618d53e33
lokesh88675 <lokeshndaair1@gmail.com>
2026-06-25 15:10:58 +0530

    Delete CLASSWORK/ARITHMETIC_OPERATOR directory

--- CLASSWORK/ARITHMETIC_OPERATOR/fuelconsuption.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program to find the average mileage of a car
-Total_distance_traveled = float(input("enter distance traveled (in KM)")) 
-Fuel_consumed =   float(input("enter FUEL CONSUMPTION (in LITERS)")) 
- # THE AVERAGE IS GIVEN BELOW IN KM\L
-Mileage = Total_distance_traveled / Fuel_consumed
-print("the Mileage is ",Mileage)
